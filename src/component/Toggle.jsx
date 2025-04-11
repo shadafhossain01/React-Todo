f02d88df5661e53c0622d32e121e45b67ea3f6e2
Shadaf Hossain <shadafhossain01@gmail.com>
2025-04-11 20:05:40 +0600

    Toggle Done

--- src/component/Toggle.jsx
+++ src/component/Toggle.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Toggle = () => {
+    const[toggle,setToggle]=useState(false)
+    const handleToggle=()=>{
+        setToggle(!toggle)
+    }
+
   return (
     <>
-        <div className='main'>
-        <div className='box'>
-        <span>Off</span>
+        <div className={`main ${toggle ? "on" : "off"}`} onClick={handleToggle}>
+        <div className={`box ${toggle ?"on-box" : "off-box"}`}>
+        <span>{toggle ? "on" :"off"}</span>
         </div>
         </div>
     </>
